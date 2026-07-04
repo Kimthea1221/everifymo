39074fd1cc07a6e150e9bbdb8e52ba3bb86d8fdc
Nicole <nicolemallari1105@gmail.com>
2026-07-05 02:37:26 +0800

    Added/fixed: extension backend folder structure.

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,6 +3,3 @@ from .users import User
 from .account_invitation_tokens import AccountInvitationToken
 from .otp_tokens import OTPToken
 from .user_sessions import UserSession
-from .consumer_accounts import ConsumerAccount
-from .complaints import Complaint
-from .verification_history import VerificationHistory
