fa52fb50e7ab6956ce2446043a5b5593a2a6b31f
Nicole <nicolemallari1105@gmail.com>
2026-07-10 13:58:03 +0800

    Add: consumer account and verification history table in models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,3 +3,6 @@ from .users import User
 from .account_invitation_tokens import AccountInvitationToken
 from .otp_tokens import OTPToken
 from .user_sessions import UserSession
+
+from .consumer_accounts import ConsumerAccount
+from .verification_history import VerificationHistory
