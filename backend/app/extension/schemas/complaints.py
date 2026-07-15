d1fda294340098d340116aa469f77c8385c80e06
Nicole <nicolemallari1105@gmail.com>
2026-07-15 22:08:36 +0800

    Connected the extension UI and the backend

--- backend/app/extension/schemas/complaints.py
+++ backend/app/extension/schemas/complaints.py
@@ -1,22 +1,22 @@
-# from pydantic import BaseModel, ConfigDict, HttpUrl
-# from datetime import datetime
-# from uuid import UUID
-# from typing import Optional
+from pydantic import BaseModel, ConfigDict, HttpUrl
+from datetime import datetime
+from uuid import UUID
+from typing import Optional
 
-# class CreateComplaint(BaseModel):
-#     product_title: str
-#     seller_name: str
-#     product_url: HttpUrl
-#     verification_result: str = "pending...(nlp not attached yet)"
+class CreateComplaint(BaseModel):
+    product_title: str
+    seller_name: str
+    product_url: HttpUrl
+    verification_result: str = "pending...(nlp not attached yet)"
 
-# class ToPrintComplaint(BaseModel):
-#     model_config = ConfigDict(from_attribute=True)
+class ToPrintComplaint(BaseModel):
+    model_config = ConfigDict(from_attribute=True)
 
-#     complaint_id: UUID
-#     case_reference: str
-#     product_title:  str
-#     product_url: Optional[str] = None
-#     seller_name: Optional[str] = None
-#     verification_result: str
-#     status: str
-#     created_at: datetime
+    complaint_id: UUID
+    case_reference: str
+    product_title:  str
+    product_url: Optional[str] = None
+    seller_name: Optional[str] = None
+    verification_result: str
+    status: str
+    created_at: datetime
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from typing import Annotated
+
 from fastapi.middleware.cors import CORSMiddleware
 
 """ from app.routers.auth.invite import router as invite_router
@@ -9,8 +11,17 @@ from app.desktop.routers.auth import registration
 from app.desktop.routers.auth.invite import router as invite_router
 from app.desktop.routers.regions.regions import router as regions_router
 
-app = FastAPI()
+from app.database.base import Base
+from app.database.sessions import engine, get_db
+from app.models import consumer_accounts
+from app.extension.routers import consumer_acc as consumer_acc_router
+from app.extension.routers import auth as auth_router
+from app.core.security import get_current_user
+from app.extension.routers import complaints
 
+
+app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,6 +35,21 @@ app.include_router(invite_router)
 app.include_router(regions_router)
 app.include_router(registration.router)
 
+app.include_router(consumer_acc_router.router)
+app.include_router(auth_router.router)
+app.include_router(complaints.router)
+
+consumer_dependency = Annotated[dict, Depends(get_current_user)]
+
 @app.get("/")
 def root():
     return {"message": "Backend is running"}
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def user(consumer: consumer_dependency):
+    if consumer is None:
+        raise HTTPException(status_code=401,
+                            detail = "Authentication Failed")
+    return {
+        "User": consumer
+    }
