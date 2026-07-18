e0822d33ed426f2a35e2d6fb1296c77ec8f6c2dd
Reyyzzz31 <2023304233@pampangastateu.edu.ph>
2026-07-18 17:50:26 +0800

    Fixing error in the dashboard file

--- desktopfrontend/src/pages/fdafolder/fda-view-reports.jsx
+++ desktopfrontend/src/pages/fdafolder/fda-view-reports.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom';
 import Sidebar from "../component/sidebar";
 import TopBar from "../component/top-bar";
 import './fda-css.css';
