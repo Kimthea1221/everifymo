735cba437055705ac3339285b65143f970f38717
code-deej <djoy.code@gmail.com>
2026-07-24 20:32:58 +0800

    feat: added more endpoints in walkin_drafts and verification_drafts router files (still incomplete)

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-""" from app.routers.auth.invite import router as invite_router
-from app.routers.regions.regions import router as regions_router """
 
 from app.desktop.routers.auth.superadmin_login import router as superadmin_login_router
 from app.desktop.routers.auth.password_reset import router as password_reset_router
 from app.desktop.routers.auth.sessions import router as sessions_router
+
+# Registration feature
 from app.desktop.routers.auth import registration
+
 from app.desktop.routers.auth.invite import router as invite_router
 from app.desktop.routers.regions.regions import router as regions_router
 from app.desktop.routers.user_management.management import router as user_management_router
+
+# Saved Drafts feature
+from app.desktop.routers.drafts.walkin_drafts import router as walkin_drafts_router
+from app.desktop.routers.drafts.all_drafts import router as all_drafts_router
+from app.desktop.routers.drafts.verification_drafts import router as verification_drafts_router
+
 
 app = FastAPI()
 
@@ -39,5 +46,8 @@ app.include_router(sessions_router)
 
 app.include_router(user_management_router)
 
+app.include_router(walkin_drafts_router)
+app.include_router(all_drafts_router)
+app.include_router(verification_drafts_router)
 
 
