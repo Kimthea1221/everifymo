943f9c81cf6b0e62149a9f0fc6339dc2efb437e2
ashantimae16 <2023304056@pampangastateu.edu.ph>
2026-08-04 22:26:12 +0800

    Feat: Add refresh token authentication and wired it to superadminuser management  and change password ui

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -34,7 +34,11 @@ class Settings(BaseSettings):
     OTP_MAX_ATTEMPTS: int = 5
     RESET_TOKEN_EXPIRE_MINUTES: int = 15
 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 180
+    
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7  # add to settings if you want this configurable
+
+    
 
     DEFAULT_EXTENSION_REGION_ID: str = ""
     
