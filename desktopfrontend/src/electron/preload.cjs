b14e48a2101e0e9d48b30dfaba1aeddfafe14974
code-deej <djoy.code@gmail.com>
2026-08-29 14:06:23 +0800

    cleanup: removed dev tools code to show up

--- desktopfrontend/src/electron/preload.cjs
+++ desktopfrontend/src/electron/preload.cjs
@@ -3,5 +3,8 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('electronAPI', {
   onDeepLinkToken: (callback) => {
     ipcRenderer.on('deep-link-token', (event, token) => callback(token))
-  }
+  },
+  removeDeepLinkToken: (callback) => {
+    ipcRenderer.removeListener("deep-link-token", callback)
+  },
 })
